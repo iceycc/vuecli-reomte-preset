ef4e7e71c722cefa98d29f3641004608df52e07b
冰洋Zz <w@icey.cc>
2020-04-25 21:08:27 +0800

    Update generator.js

--- generator.js
+++ generator.js
@@ -3,14 +3,10 @@ module.exports = (api, options, rootOptions) => {
     api.extendPackage({
       scripts: {
         test: 'vue-cli-service test',
-        zgTest: 'zg-test'
+        test2: 'zg-test'
       }
     })
   
     // 复制并用 ejs 渲染 `./template` 内所有的文件
-    api.render('./template')
-  
-    if (options.foo) {
-      // 有条件地生成文件
-    }
+    // api.render('./template')
   }
